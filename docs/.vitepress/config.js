11b87e160745efd48078ea07ee03fe243f1bb42b
kyrylo.buchynskyi <kyrylo.buchynskyi@danads.se>
2023-08-04 10:13:43 +0300

    feat: upgrade to vitepress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  lang: 'en-US',
+  title: 'AirGrid Docs',
+  description: 'AirGrid developer documentation site.',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.png' }]
+  ],
+  cleanUrls: true,
+  appearance: false,
+
+  // Theme Config
+  themeConfig: {
+    logo: '/logo.png',
+    search: {
+      provider: 'local'
+    },
+    siteTitle: false,
+    sidebar: [
+      {
+        text: 'Introduction',
+        link: '/',
+      },
+      {
+        text: 'Advertisers',
+        link: '/advertisers/',
+      },
+      {
+        text: 'Publishers',
+        link: '/publishers/',
+      },
+    ]
+  },
+}
